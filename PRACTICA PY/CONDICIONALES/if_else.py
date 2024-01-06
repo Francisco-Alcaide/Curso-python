e4e2bf90085f1352872cdda7041b6e2a9deaf3a6
Francisco Alcaide <francy25.fjaa@gmail.com>
2024-01-06 13:09:55 -0300

    CONDICIONALES
    DIA 2 PRACTIANDO

---
+++ PRACTICA PY/CONDICIONALES/if_else.py
@@ -0,0 +1,7 @@
+contrasena_almacenada = "FranciscoAlcaide"
+contrasena_escrita = "FranciscoAlcaide"
+
+if contrasena_almacenada == contrasena_escrita:
+    print("INICIO DE SESION...")
+else:
+    print("CONTRASENA EQUIVOCADA, INTENTE DE NUEVO")
---
+++ PRACTICA PY/OPERADORES/op_comparacion.py
@@ -0,0 +1,28 @@
+
+igual_que = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+#calculos combinados
+
+a = 5
+b = 10
+c = 20
+
+comparacion = a + b < c
+
+#comnparar usuarios
+ 
+contrasena_almacenada = "FranciscoAlcaide"
+contrasena_escrita = "FranciscoAlcaide"
+
+
+print(contrasena_almacenada == contrasena_escrita)
